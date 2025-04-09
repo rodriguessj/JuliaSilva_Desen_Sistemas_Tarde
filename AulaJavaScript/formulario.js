ae94aa1b5558ed6d98fcf1ea6f710bed2de98ff2
rodriguessj <roiz.rodr1gues23@gmail.com>
2025-04-08 23:03:10 -0300

    Update formulario.js
    Mascaras

--- AulaJavaScript/formulario.js
+++ AulaJavaScript/formulario.js
@@ -1,61 +1,61 @@
 //EXECUTAR MASCARAS
 //DEFINE O OBJETO E CHAMA FUNÇÃO
-function mascara(o,f){
-    objeto=o
-    funcao=f
-    setTimeout("executaMascara()",1)
-
+function mascara(o, f) {
+    objeto = o;
+    funcao = f;
+    setTimeout("executaMascara()", 1);
 }
 
 function executaMascara() {
-    objeto.value=funcao(objeto.value)
+    objeto.value = funcao(objeto.value);
 }
 
 //MASCARAS
 
 //MASCARA DO TELEFONE
+function telefone(variavel) {
+    variavel = variavel.replace(/\D/g, ""); // REMOVE TUDO QUE NÃO É DIGITO
 
-function telefone(variavel){
-    variavel=variavel.replace(/\D/g,"")//REMOVE TUDO QUE NÃO É DIGITO
+    // ADICIONA PARENTESES EM VOLTA DOS DOIS PRIMEIROS DIGITOS
+    variavel = variavel.replace(/^(\d{2})(\d)/, "($1)$2");
 
-    //ADICIONA PARENTESES EM VOLTA DOS DOIS PRIMEIROS DIGITOS
-    variavel=variavel.replace(/^(\d\d)(\d)/g,"($1)$2")
+    // ADICIONA HIFEM ENTRE O QUARTO E QUINTO NUMERO
+    variavel = variavel.replace(/(\d{4})(\d)/, "$1-$2");
 
-    //ADICIONA HIFEM ENTRE O QUARTO E QUINTO NUMERO
-    variavel=variavel.replace(/(\d{4})(\d)/,"$1-$2")
+    return variavel; 
 }
 
 //MASCARA DO RG E CPF
+function RGeCPF(variavel) {
+    variavel = variavel.replace(/\D/g, ""); // REMOVE TUDO QUE NAO É NUMERO
 
-function RGeCPF(variavel){
-    variavel=variavel.replace(/\D/g,"")//REMOVE TUDO QUE NAO É NUMERO
+    // COLOCA UM PONTO APOS O TERCEIRO DIGITO
+    variavel = variavel.replace(/(\d{3})(\d)/, "$1.$2");
 
-    //COLOCA UM PONTO APOS O TERCEIRO DIGITO E O QUARTO DIGITO
-    variavel=variavel.replace(/(\{3})(\d)/,"$1.$2")
+    // COLOCA UM PONTO APOS O SEXTO DIGITO
+    variavel = variavel.replace(/(\d{3})(\d)/, "$1.$2");
 
-    //COLOCA UM PONTO APOS O SEXTO DIGITO E O SETIMO DIGITO
-    variavel=variavel.replace(/(\{3})(\d)/,"$1.$2")
+    // COLOCA HIFEN APÓS O SEXTO DIGITO OU SETIMO DIGITO 
+    variavel = variavel.replace(/(\d{3})(\d{1,2})$/, "$1-$2");
 
-    //COLOCO HIFEM APÓS O SETIMO DIGITO E PERMITE APENAS 2 DIGITOS APOS O HIFEM
-    variavel=variavel.replace(/(\d{3})(\d{1,2})$/,"$1-$2")
+    return variavel; 
 }
 
 //MASCARA DO CEP
+function cep(variavel) {
+    variavel = variavel.replace(/\D/g, ""); // REMOVE TUDO QUE NAO É NUMERO
 
-function cep(variavel){
-    variavel=variavel.replace(/\D/g,"")//REMOVE TUDO QUE NAO É NUMERO
+    // ADICIONA O HÍFEN NA MÁSCARA DE CEP
+    variavel = variavel.replace(/(\d{5})(\d{1,3})$/, "$1-$2");
 
-    variavel=variavel.replace(/(\d{2})(\d)/,"$1.$2")//COLOCA SEGUNDA BARRA
-
-    variavel=variavel.replace(/(\d{3})(\d{1,3})$/,"$1.$2")
-    return variavel
-
+    return variavel; 
 }
 
 //MASCARA DATA
+function data(variavel) {
+    variavel = variavel.replace(/\D/g, ""); // REMOVE TUDO QUE NAO É NUMERO
+    variavel = variavel.replace(/(\d{2})(\d)/, "$1/$2"); // COLOCA A PRIMEIRA BARRA
+    variavel = variavel.replace(/(\d{2})(\d)/, "$1/$2"); // COLOCA A SEGUNDA BARRA
 
-function data(variavel){
-    variavel=variavel.replace(/\D/g,"")//REMOVE TUDO QUE NAO É NUMERO
-    variavel=variavel.replace(/(\d{2})(\d)/,"$1.$2")//COLOCA SEGUNDA BARRA
-    variavel=variavel.replace(/(\d{2})(\d)/,"$1.$2")//COLOCA SEGUNDA BARRA
+    return variavel; 
 }
