c78e31702923938d789e5466238c2782d4e1d259
rodriguessj <roiz.rodr1gues23@gmail.com>
2025-04-15 17:03:33 -0300

    Create exemploarray.php

---
+++ PHP/exemploarray.php
@@ -0,0 +1,13 @@
+<?php
+  $dias=array('domingo','segunda','terça','quarta','quinta','sexta','sabado');
+  echo $dias[1]."<br/>";
+  print_r($dias);
+  echo "<br/>";
+  var_dump($dias);
+?>
+
+<?php
+      echo "<h2 align='center'>
+            Julia Silva
+            </h2>";
+?>
