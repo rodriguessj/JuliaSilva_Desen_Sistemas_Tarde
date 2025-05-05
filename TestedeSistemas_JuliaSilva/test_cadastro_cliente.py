61e0a3fd82479eb94cbf8d8750310348927a60a5
rodriguessj <roiz.rodr1gues23@gmail.com>
2025-05-05 15:21:37 -0300

    Update test_cadastro_cliente.py

--- TestedeSistemas_JuliaSilva/test_cadastro_cliente.py
+++ TestedeSistemas_JuliaSilva/test_cadastro_cliente.py
@@ -1,37 +1,38 @@
 from selenium import webdriver
-from selenium.webdriver.commom.by import By
+from selenium.webdriver.common.by import By 
 import time
 
 #Configuração do WebDriver (nesse exemplo, estamos usando o Chrome)
 driver = webdriver.Chrome()
 
-#Acessa página de cadastro usando o caminho absoluto com o protocolo file://
-#Certifique-se de que o caminho está apontando para um arquivo HTML específico
+#Acessa a página de cadastro usando o caminho absoluto
+# com o protocolo file://
+# Certifique-se de que o caminho está apontando para um arquivo
+# HTML específico
+driver.get("file:///C:/Users/julia_silva19/Documents/GitHub/JuliaSilva_Desen_Sistemas_Tarde/TestedeSistemas_JuliaSilva/index.html")
 
-driver.get("file:///C:\Users\julia_silva19\Documents\GitHub\JuliaSilva_Desen_Sistemas_Tarde\TestedeSistemas_JuliaSilva\index.html")
-
-#Preenche o campo Nome
+# Preencha o campo nome
 nome_input = driver.find_element(By.ID, "name")
-nome_input.send_keys("João da Silva")
+nome_input.send_keys("João Silva")
 
-#Preenche o campo CPF
+# Preencha o campo CPF
 cpf_input = driver.find_element(By.ID, "cpf")
 cpf_input.send_keys("12345678901")
 
-#Preenche campo endereço
-endereço_input = driver.find_element(By.ID, "address")
-endereço_input.send_keys("Rua das Flores, 123")
+# Preencha o campo Endereço
+endereco_input = driver.find_element(By.ID, "address")
+endereco_input.send_keys("Rua das Flores")
 
-#Preenche o campo telefone
+# Preencha o campo Telefone
 telefone_input = driver.find_element(By.ID, "phone")
-telefone_input.send_keys("11987654321")
+telefone_input.send_keys("119887654321")
 
-#Clica no botão de cadastrar
-submit_button = driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
-submit_button.click()
+#Clica no botão de Cadastrar
+submit_buttom= driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
+submit_buttom.click()
 
-#Aguarda um momento para vizualizar o resultado (em uma aplicação real, você verificaria a resposta)
-time.sleep(3)
+#Aguarde um momento para visualizar o resultado (em uma aplicação real, você verificaria a resposta)
+time.sleep(600)
 
 #Fecha o navegador
 driver.quit()
